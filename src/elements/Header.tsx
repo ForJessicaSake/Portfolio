016a4bd250ecb3cc112cf598d17b379014a54050
Jessica Joseph <jessicajoseph2019@yahoo.com>
2023-02-16 06:39:58 +0100

    feat:completed the figcaption of the  header section

--- src/elements/Header.tsx
+++ src/elements/Header.tsx
@@ -3,9 +3,9 @@ import block from "../assets/block.svg";
 const Header = () => {
   return (
     <header className="flex lg:flex-row flex-col justify-between py-16 px-6">
-      <figcaption className="w-6/12">
+      <figcaption className="lg:w-6/12 w-full">
         <h1 className="text-3xl font-semibold">Hi, I'm Jessica – a <span className="text-primary">Frontend Engineer</span> and <span className="text-primary">Technical Writer</span></h1>
-        <p className="py-6 text-sm">
+        <p className="py-6 text-sm text-justify">
           I'm currently developing interactive web applications using the latest
           web technologies. In my spare time, I enjoy exploring the world of
           JavaScript and Backend-as-a-Service to create cool applications, and I
@@ -14,7 +14,7 @@ const Header = () => {
         </p>
         <Button text="contact me!!" />
       </figcaption>
-      <figure className="w-5/12">
+      <figure className="lg:w-5/12 w-full lg:pt-0 py-10">
         <img src={block} alt="" />
       </figure>
     </header>
