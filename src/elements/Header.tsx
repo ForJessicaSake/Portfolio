e4a80a89aec4bff72d105805b4870c8d1b1bbaee
Jessica Joseph <jessicajoseph2019@yahoo.com>
2023-02-17 01:06:48 +0100

    feat:header completed

--- src/elements/Header.tsx
+++ src/elements/Header.tsx
@@ -1,8 +1,10 @@
 import Button from "../custom/Button";
 import block from "../assets/block.svg";
+import avartar from "../assets/header.png"
+import dots from "../assets/Dots.svg"
 const Header = () => {
   return (
-    <header className="flex lg:flex-row flex-col justify-between py-16 px-6">
+    <header className="flex lg:flex-row flex-col lg:items-center justify-between py-16 px-6">
       <figcaption className="lg:w-6/12 w-full">
         <h1 className="text-3xl font-semibold">Hi, I'm Jessica – a <span className="text-primary">Frontend Engineer</span> and <span className="text-primary">Technical Writer</span></h1>
         <p className="py-6 text-sm text-justify">
@@ -14,8 +16,11 @@ const Header = () => {
         </p>
         <Button text="contact me!!" />
       </figcaption>
-      <figure className="lg:w-5/12 w-full lg:pt-0 py-10">
-        <img src={block} alt="" />
+      <figure className="lg:w-4/12 w-full lg:pt-0 py-16 flex justify-between lg:items-center lg:px-8 relative">
+        <img src={block} alt="block" />
+        <img src={avartar} alt="" className="lg:w-48 w-44 absolute lg:left-40 left-36 bottom-6 lg:bottom-0" />
+        <img src={dots} alt="dots" className="relative lg:top-20 lg:left-4 top-14"/>
+
       </figure>
     </header>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import Header from '../elements/Header'
 
 const Home:React.FC = () => {
   return (
-    <main className='bg-background font-font text-secondary'>
+    <main className='bg-background font-font text-secondary px-2'>
     <Navbar/>
     <Header/>
     </main>
