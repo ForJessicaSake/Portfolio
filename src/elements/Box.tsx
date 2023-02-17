9d5a38778edef7940bc1b55937be8e59f49b7a58
Jessica Joseph <jessicajoseph2019@yahoo.com>
2023-02-17 15:35:30 +0100

    feat:skill section completed

--- src/elements/Box.tsx
+++ src/elements/Box.tsx
@@ -3,18 +3,18 @@ const Box = () => {
   return (
     <hgroup>
       <section className="flex justify-center relative">
-        <figure className="absolute lg:left-52 left-10 bottom-12">
+        <figure className="absolute lg:left-52 left-10 lg:bottom-12 bottom-20">
             <img src ={quotes} alt ="quotes" className="lg:h-8 lg:w-8 w-7 h-7"/>
         </figure>
-        <h3 className="border border-dotted border-secondary h-16 flex justify-center items-center lg:w-8/12 w-10/12 leading-8 lg:text-base text-xs px-1 lg:pl-0 pl-6">
+        <h3 className="border border-dotted border-secondary lg:h-16 h-24 flex justify-center items-center lg:w-8/12 w-10/12 leading-8 lg:text-base text-xs px-1 lg:pl-0 pl-6">
           Don't ask questions you don't wanna know. Learned my lesson, way too long ago.
         </h3>
-        <figure className="absolute lg:right-52 right-12 top-12">
+        <figure className="absolute lg:right-52 right-12 lg:top-12 top-20">
             <img src ={quotes} alt ="quotes" className="lg:h-8 lg:w-8 w-7 h-7"/>
         </figure>
       </section>
       <section className="flex justify-end lg:w-10/12 w-11/12">
-        <h3 className="border border-t-0 border-dotted border-secondary h-11 flex justify-center items-center lg:w-2/12 w-4/12 lg:text-base text-xs px-1">
+        <h3 className="border border-t-0 border-dotted border-secondary h-11 flex justify-center items-center lg:w-2/12 w-5/12 lg:text-base text-xs px-1">
          - Billie Eilish
         </h3>
       </section>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ import Header from '../elements/Header'
 
 const Home:React.FC = () => {
   return (
-    <main className='bg-background font-font text-secondary px-2'>
+    <main className='bg-background font-font text-secondary px-2 overflow-x-hidden'>
     <Navbar/>
     <Header/>
     <Box/>
