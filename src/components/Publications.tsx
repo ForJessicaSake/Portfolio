72c99db6028b6fd725bd0de0ebe7a51132aacde6
Jessica Joseph <jessicajoseph2019@yahoo.com>
2023-02-18 17:09:10 +0100

    fix: blog links and project links work now

--- src/components/Publications.tsx
+++ src/components/Publications.tsx
@@ -1,9 +1,10 @@
 import { useCallback } from "react";
-import {page} from "../atoms/Atoms"
+import { page } from "../atoms/Atoms";
 import { useQuery } from "react-query";
 import { useAtom } from "jotai";
-import stroke from "../assets/stroke.svg" 
+import stroke from "../assets/stroke.svg";
 import Button from "../custom/Button";
+import { Link } from "react-router-dom";
 
 type ArticleType = {
   id: number;
@@ -14,7 +15,7 @@ type ArticleType = {
 };
 
 const Publications = () => {
-  const variables = { page: 0};
+  const variables = { page: 0 };
   const query = `
     query GetUserArticles($page: Int!) {
         user(username: "ForJessicasake") {
@@ -44,69 +45,71 @@ const Publications = () => {
 
     const result = await data.json();
     const post = result.data.user.publication.posts;
-    console.log(post)
+    console.log(post);
 
     return post; // Ensure to return the data from the function
   };
-const [pages, setPages] = useAtom(page)
-const { data, status } = useQuery<ArticleType[]>(["page", pages], fetchData,  {
-  keepPreviousData: true,
-});
+  const [pages, setPages] = useAtom(page);
+  const { data, status } = useQuery<ArticleType[]>(["page", pages], fetchData, {
+    keepPreviousData: true,
+  });
 
-const handlePage = () => {
-  setPages(pages + 1);
-};
+  const handlePage = () => {
+    setPages(pages + 1);
+  };
 
-
-return (
+  return (
     <section className="flex flex-col px-6 pb-20" id="publications">
-       <section className="flex justify-start items-center lg:w-5/12 w-72 pb-10">
+      <section className="flex justify-start items-center lg:w-5/12 w-72 pb-10">
         <h1 className="lg:text-2xl text-xl">
           <span className="text-primary ">#</span>Publications
         </h1>
         <figure className="pl-4">
-        <img src={stroke} alt="line"className="lg:w-20 w-32" />
+          <img src={stroke} alt="line" className="lg:w-20 w-32" />
         </figure>
       </section>
-    <section className="flex place-items-center w-full">
-    {status === "error" && (
-        <p className="text-sm font-light text-center">Error in fetching data</p>
-      )}
-      {status === "loading" && (
-        <p className="text-sm font-light text-center">...</p>
-      )}
-      {status === "success" && (
-    <div className="grid grid-col-1 lg:grid-cols-3 lg:gap-5 gap-6 place-items-center w-full">
+      <section className="flex place-items-center w-full">
+        {status === "error" && (
+          <p className="text-sm font-light text-center">
+            Error in fetching data
+          </p>
+        )}
+        {status === "loading" && (
+          <p className="text-sm font-light text-center">...</p>
+        )}
+        {status === "success" && (
+          <div className="grid grid-col-1 lg:grid-cols-3 lg:gap-5 gap-6 place-items-center w-full">
+            {data?.map((articles: ArticleType) => (
+              <article
+                key={articles.id}
+                className="items-center text-justify cursor-pointer hover:shadow-2xl shadow-lg"
+              >
+                <figure className="border w-80">
+                  <Link
+                    to={`https://forjessicasake.hashnode.dev/${articles.slug}`}
+                  >
+                    <img
+                      src={articles.coverImage}
+                      alt="blog post cover"
+                      className=" w-80 h-36 object-fill"
+                    />
+                  </Link>
+                </figure>
 
-      {data?.map((articles:ArticleType) => (
-          <article
-            key={articles.id}
-            className="items-center text-justify cursor-pointer hover:shadow-2xl shadow-lg"
-          >
-            <figure className="border w-80">
-            <img
-              src={articles.coverImage}
-              alt="blog post cover"
-              className=" w-80 h-36 object-fill"
-            />
-            </figure>
-       
-              <figcaption className="w-80">
-              <a href={`https://forjessicasake.hashnode.dev/${articles.slug}`}>
-                <h2 className="font-bold h-14 items-center lg:text-sm text-sm border-t-0 border px-2 py-2 text-justify">
-                  {articles.title}
-                </h2>
-                </a>
-                <p className="h-32 text-xs border border-t-0 px-2 py-1 text-justify">{articles.brief}</p>
-              </figcaption>
-          </article>
-        ))}
+                <figcaption className="w-80">
+                  <h2 className="font-bold h-14 items-center lg:text-sm text-sm border-t-0 border px-2 py-2 text-justify">
+                    {articles.title}
+                  </h2>
+                  <p className="h-32 text-xs border border-t-0 px-2 py-1 text-justify">
+                    {articles.brief}
+                  </p>
+                </figcaption>
+              </article>
+            ))}
           </div>
-          )}
-
+        )}
+      </section>
     </section>
-</section>
-
   );
 };
 
--- src/elements/Card.tsx
+++ src/elements/Card.tsx
@@ -1,15 +1,19 @@
+import { Link } from "react-router-dom";
 import Button from "../custom/Button";
 
 type Project = {
   image: any;
   title: string;
   about: string;
+  link:string;
 };
 
-const Card = ({ image, title, about }: Project) => {
+const Card = ({ image, title, about, link}: Project) => {
   return (
     <section className="border w-80 cursor-pointer">
+     <Link to={link}>
       <img src={image} alt="" />
+      </Link>
       <h2 className="font-bold text-justify flex items-center h-14 lg:text-sm text-sm border-t-0 border px-2 py-2">
         {title}
       </h2>
