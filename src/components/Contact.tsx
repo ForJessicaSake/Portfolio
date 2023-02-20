5200717d63c69e9e0f34a625294b13b05143c95d
Jessica Joseph <jessicajoseph2019@yahoo.com>
2023-02-20 09:13:04 +0100

    fix: boxquotes

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -8,7 +8,7 @@ const Contact = () => {
   return (
     <section className="flex flex-col px-6 pb-10" id="contact">
       <section className="flex justify-around items-center lg:w-3/12 w-72">
-      <h1 className="lg:text-2xl text-xl">
+        <h1 className="lg:text-2xl text-xl">
           <span className="text-primary ">#</span>Contact
         </h1>
         <figure className="pl-4">
@@ -18,35 +18,52 @@ const Contact = () => {
       <section className="flex lg:flex-row flex-col justify-between text-sm">
         <div className="w-full lg:w-6/12 lg:pb-0 pb-6 text-justify mt-4">
           <p className="leading-6">
-            I'm currently on the lookout for exciting opportunities, whether
-            it's full-time, contract, or freelance work. If you think I could be
-            a good fit for a job you have in mind, please don't hesitate to
-            reach out to me. I'm open and ready to explore any potential
-            opportunities!
+            I'm on the lookout for some new opportunities to flex my skills and
+            expertise. If you're in need of a go-getter who's not afraid to roll
+            up their sleeves and tackle any challenge, then look no further!
+            Whether you're looking for a full-time team member, a contractor, or
+            someone to collaborate with on a freelance basis, I'm game. Let's
+            chat about how I can help take your business to the next level.
+            Shoot me a message and let's make it happen!
           </p>
         </div>
         <div className="w-full lg:w-3/12 leading-10 border px-4 lg:mb-0 mb-5 mt-4">
           <h3 className="text-white">Message me here</h3>
           <figure className="flex w-64 justify-between items-center">
-            <img src={email} alt="email" className ="h-5 w-5"/>
+            <img src={email} alt="email" className="h-5 w-5" />
             jessicajoseph2019@yahoo.com
           </figure>
-          <figure >
-          <Link to="https://twitter.com/forJessica_sake" className="flex w-36 justify-between items-center">
-            <FiTwitter className="h-5 w-5" />
-            forJessicasake
-          </Link> 
+          <figure>
+            <Link
+              to="https://twitter.com/forJessica_sake"
+              className="flex w-36 justify-between items-center"
+            >
+              <FiTwitter className="h-5 w-5" />
+              forJessicasake
+            </Link>
           </figure>
         </div>
       </section>
       <form className="flex flex-col lg:w-6/12 w-full">
         <section className="flex lg:flex-row flex-col  justify-between my-4">
-            <input placeholder="Name" className="bg-background border h-10 px-2 py-2 lg:w-72 w-full"/>
-            <input placeholder="Email" className="bg-background border  h-10 px-2 py-2 lg:mt-0 mt-4 lg:w-64 w-full"/>
+          <input
+            placeholder="Name"
+            className="bg-background border h-10 px-2 py-2 lg:w-72 w-full"
+          />
+          <input
+            placeholder="Email"
+            className="bg-background border  h-10 px-2 py-2 lg:mt-0 mt-4 lg:w-64 w-full"
+          />
         </section>
-        <input placeholder="Title" className="bg-background border h-10 px-2 py-2 lg:w-full w-full"/>
-        <textarea placeholder="Message" className="bg-background border h-28 flex items-start justify-start px-2 mb-6 my-5 lg:w-full w-full"></textarea>
-        <Button text="Send"/>
+        <input
+          placeholder="Title"
+          className="bg-background border h-10 px-2 py-2 lg:w-full w-full"
+        />
+        <textarea
+          placeholder="Message"
+          className="bg-background border h-28 flex items-start justify-start px-2 mb-6 my-5 lg:w-full w-full"
+        ></textarea>
+        <Button text="Send" />
       </form>
     </section>
   );
