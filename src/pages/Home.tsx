b79b478772cc2e661cd947e15e17e946e7a23e5e
Jessica Joseph <jessicajoseph2019@yahoo.com>
2023-02-20 22:34:52 +0100

    feat:added smooth scroll

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,7 +11,7 @@ import Experience from '../components/Experience'
 
 const Home:React.FC = () => {
   return (
-    <main className='bg-background font-font text-secondary px-2 overflow-x-hidden scroll-smooth'>
+    <main className='bg-background font-font text-secondary px-2 overflow-x-hidden'>
     <Navbar/>
     <Header/>
     <Box/>
