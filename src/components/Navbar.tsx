c3e28f1dc02b948aeca5627c4b9fde8cc2ba726f
Jessica Joseph <jessicajoseph2019@yahoo.com>
2023-02-20 15:48:13 +0100

    fix:nav is now fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar: React.FC = () => {
     setNav(!nav)
   } 
   return (
-    <nav className="flex justify-between items-center h-20 bg-background cursor-pointer"> 
+    <nav className="flex justify-between items-center lg:h-20 h-20 border-20 bg-background cursor-pointer fixed w-full z-20"> 
       <section className="flex text-white justify-between w-full items-center px-6">
         <figure className="flex items-center lg:w-24 w-28 justify-between">
           <img src={Logo} alt="Logo" className="w-6 h-6 lg:w-5 lg:h-5"/>
@@ -21,7 +21,7 @@ const Navbar: React.FC = () => {
         </figure>
         { nav ? <img src={Cancel} alt="cancel" className="w-6 h-6 lg:hidden block" onClick={navClose} /> : <img src={Menu} alt="menu" className="w-6 h-6 lg:hidden block" onClick={navClose} />  }
       </section>
-      <ul onClick={navClose} className={`${nav ? "block": "hidden"} px-6 z-50 text-secondary absolute lg:text-sm lg:static lg:flex lg:flex-row lg:h-0 lg:top-0  top-20 py-8 bg-background w-full h-full flex flex-col  justify-between`}>
+      <ul onClick={navClose} className={`${nav ? "block": "hidden"} px-6 z-50 text-secondary absolute lg:text-sm lg:static lg:flex lg:flex-row lg:h-0 lg:top-0 h-96 top-20 py-8 lg:py-0 bg-background w-full flex flex-col  justify-between`}>
         <li>
           <Link to ="/" className="active:text-white">
             <span className="text-primary">#</span>about-me
