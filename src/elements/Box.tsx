41617dafffbcbeba016ae27d226d9f56852ef4bc
Jessica Joseph <jessicajoseph2019@yahoo.com>
2023-02-20 09:09:37 +0100

    fix: boxquotes

--- src/elements/Box.tsx
+++ src/elements/Box.tsx
@@ -7,7 +7,7 @@ const Box = () => {
           <img src={quotes} alt="quotes" className="lg:h-8 lg:w-8 w-7 h-7" />
         </figure>
         <h3 className="border border-dotted border-secondary lg:h-16 h-24 flex justify-center items-center lg:w-8/12 w-10/12 leading-8 lg:text-base text-xs px-1 lg:pl-0 pl-6">
-          I still don't know what to write here, but i will come up with something.
+             Cause everyone wants something from me now and I don't want to let em down.
         </h3>
         <figure className="absolute lg:right-52 right-12 lg:top-12 top-20">
           <img src={quotes} alt="quotes" className="lg:h-8 lg:w-8 w-7 h-7" />
@@ -15,7 +15,7 @@ const Box = () => {
       </section>
       <section className="flex justify-end lg:w-10/12 w-11/12">
         <h3 className="border border-t-0 border-dotted border-secondary h-11 flex justify-center items-center lg:w-2/12 w-5/12 lg:text-base text-xs px-1">
-          - Artiste
+          - Billie Eilish
         </h3>
       </section>
     </hgroup>
