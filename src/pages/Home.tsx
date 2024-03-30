6e72e783469c7ee95a273826d112a91ef62bc147
Jessica Joseph <jessicajoseph2019@yahoo.com>
2024-03-30 17:04:48 +0100

    chore: minor

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,9 @@
 import Contact from '../components/Contact'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
-import Publications from '../components/Publications'
 import Skills from '../components/Skills'
-import Box from '../elements/Box'
 import Header from '../components/Header'
 import Project from '../components/Project'
-import WritingExperience from '../components/Writing'
-import Experience from '../components/Experience'
 import GuestWriting from '../components/GuestWriting'
 
 const Home:React.FC = () => {
@@ -15,13 +11,9 @@ const Home:React.FC = () => {
     <main className='2xl:container 2xl:mx-auto bg-black font-poppins text-secondary px-2 overflow-x-hidden'>
     <Navbar/>
     <Header/>
-    {/* <Box/> */}
     <Skills/>
-    {/* <Experience/> */}
     <Project/>
-    {/* <WritingExperience/> */}
     <GuestWriting/>
-    <Publications/>
     <Contact/>
     <Footer/>
     </main>
